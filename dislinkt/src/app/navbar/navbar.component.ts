8e9c8f8a451ebd6eb744f1e6ffd80d9bbfba9d8a
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2022-07-04 23:33:55 +0200

    feat: post notification

--- dislinkt/src/app/navbar/navbar.component.ts
+++ dislinkt/src/app/navbar/navbar.component.ts
@@ -64,6 +64,10 @@ export class NavbarComponent implements OnInit {
       "/user/" + this.currentUserId + "/queue/messages",
       this.onMessageReceived
     );
+    this.stompClient.subscribe(
+      "/user/2/queue/posts",
+      this.onMessageReceivedPost
+    );
   };
 
    onError = (err: any) => {
@@ -94,6 +98,27 @@ export class NavbarComponent implements OnInit {
      else {
       alert("You've received  a new message!");
     }
+  };
+
+  onMessageReceivedPost= (msg: { body: string; }) => {
+    const notification = JSON.parse(msg.body);
+    console.log(notification)
+    /*const active = JSON.parse(sessionStorage.getItem("recoil-persist"))
+      .chatActiveContact;
+
+    if (active.id === notification.senderId) {
+      findChatMessage(notification.id).then((message) => {
+        const newMessages = JSON.parse(sessionStorage.getItem("recoil-persist"))
+          .chatMessages;
+        newMessages.push(message);
+        setMessages(newMessages);
+      });
+    } else {
+      message.info("Received a new message from " + notification.senderName);
+    }
+    loadContacts();*/
+      if(notification.id != this.currentUserId)
+      alert("One of the people you follow has created a new post.");
   };
 
 }
